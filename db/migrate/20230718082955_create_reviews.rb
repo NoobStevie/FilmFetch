583e638fb29457f01ceaf3fd16f77aa78a42e585
Stevie <kamaustevek78@gmail.com>
2023-07-18 11:58:00 +0300

    Databases Updated

--- db/migrate/20230718082955_create_reviews.rb
+++ db/migrate/20230718082955_create_reviews.rb
@@ -1,6 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[7.0]
   def change
     create_table :reviews do |t|
+      t.string :review_content
+      t.integer :user_id
+      t.integer :movie_id
 
       t.timestamps
     end
